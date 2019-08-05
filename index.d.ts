c62563b2ec8cd520a4006406600c3b184af45199
大板栗 <576839360@qq.com>
2019-08-05 10:31:18 +0800

    ⬆️ Update types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
-declare namespace common {
-    function type(target: any): string
-    function isPlaintarget(target: any): boolean
-    function extend(deep: boolean, target: any, srouce: any): object
-    function extend(target: any, srouce: any): object
-}
+export const type: (target: any) => void
+export const isPlaintarget: (target: any) => void
+export function extend(deep: boolean, target: any, srouce: any): void
+export function extend(target: any, srouce: any): void
