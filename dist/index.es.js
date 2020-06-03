e2ea394112b5aacdbdcec4d48f24610368d10d9f
大板栗 <576839360@qq.com>
2020-06-03 15:13:57 +0800

    📝 Add functions

--- dist/index.es.js
+++ dist/index.es.js
@@ -556,5 +556,103 @@ function showPerformance(enables) {
     }); };
 }
 
-export { event, eventify, extend, showPerformance, showPerformancePannel, usesify };
+function getQuery(name) {
+    var search = location.search;
+    var hasSearch = search !== '';
+    var queryString = hasSearch ? search.slice(1) : '';
+    var queries = queryString.split('&');
+    var query = queries.reduce(function (prev, current) {
+        var _a = current.split('='), left = _a[0], right = _a[1];
+        prev[left] = decodeURIComponent(right);
+        return prev;
+    }, {});
+    return query[name];
+}
+function isEmpty(target) {
+    if (typeof target === 'object') {
+        return Object.keys(target).length === 0;
+    }
+    else if (Array.isArray(target) || typeof target === 'string') {
+        return target.length === 0;
+    }
+    else {
+        return false;
+    }
+}
+function type(object) {
+    var class2type = {};
+    var type = class2type.toString.call(object);
+    var typeString = 'Boolean Number String Function Array Date RegExp Object Error Symbol';
+    if (object == null)
+        return object + '';
+    typeString.split(' ').forEach(function (type) {
+        class2type["[object " + type + "]"] = type.toLowerCase();
+    });
+    var isObject = typeof object === 'object';
+    var isFn = typeof object === 'function';
+    return isObject || isFn ? class2type[type] || 'object' : typeof object;
+}
+function forin(object, fn) {
+    if (typeof object === 'object') {
+        for (var key in object) {
+            if (object.hasOwnProperty(key)) {
+                var value = object[key];
+                fn(key, value);
+            }
+        }
+    }
+}
+var getValue = function (root, get) {
+    if (type(root) !== 'object')
+        return root;
+    var value = root;
+    var keyArr = get.split('.');
+    for (var i = 0, l = keyArr.length; i < l; i++) {
+        var v = keyArr[i];
+        if (v) {
+            if (value[v]) {
+                value = value[v];
+            }
+            else {
+                value = undefined;
+                break;
+            }
+        }
+    }
+    return value;
+};
+var setValue = function (tar, key, value) {
+    if (type(tar) !== 'object') {
+        throw new Error('setValue tar muse be a object!');
+    }
+    else {
+        var pIndex = key.trim().indexOf('.');
+        if (pIndex > 0 && pIndex < key.length - 1) {
+            var keyArr = key.trim().split('.');
+            var _obj = tar;
+            var wrongKey = '';
+            for (var i = 0, l = keyArr.length - 1; i < l; i++) {
+                var v = keyArr[i];
+                if (typeof _obj[v] === 'object') {
+                    _obj = _obj[v];
+                }
+                else if (typeof _obj[v] === 'undefined') {
+                    wrongKey = v;
+                }
+            }
+            if (!wrongKey) {
+                var lastKey = keyArr[keyArr.length - 1];
+                _obj[lastKey] = value;
+            }
+            else {
+                throw new Error("the key(" + wrongKey + ") is not in tar obj!");
+            }
+        }
+        else {
+            tar[key.replace(/\./g, '')] = value;
+        }
+    }
+};
+
+export { event, eventify, extend, forin, getQuery, getValue, isEmpty, setValue, showPerformance, showPerformancePannel, type, usesify };
 //# sourceMappingURL=index.es.js.map
